0e9cb158ea9d5b29d741d812d93cc5d526b9d98c
Kavitha-Patali <kavithapatali@gmail.com>
2024-03-13 15:32:55 +0530

    Assignment 38

---
+++ Asgmnt38_stringfunctionreplace.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class Asgmnt38_stringfunctionreplace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 String name="Kavitha patali";
+		String Ans= name.replaceAll("patali", "bangalore");
+		System.out.println(Ans);
+	}
+
+}
