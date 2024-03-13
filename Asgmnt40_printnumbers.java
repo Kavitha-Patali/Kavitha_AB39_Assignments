7f6db5498c754051fc4a1f5a515d4ccc81bdd348
Kavitha-Patali <kavithapatali@gmail.com>
2024-03-13 15:33:45 +0530

    Assignment 40

---
+++ Asgmnt40_printnumbers.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class Asgmnt40_printnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		for(int i=-4;i>-1000;i--)
+		
+		{
+		if(i%4==0)
+		{
+		System.out.println(i);	
+	}
+
+}
+}
+}
